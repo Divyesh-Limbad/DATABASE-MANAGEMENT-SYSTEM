baebda01eb818c560a466cd8af075b14411a2f6e
Divyesh Limbad <divyeshlimbad90@gmail.com>
2024-12-14 10:22:49 +0530

    Update Lab-10.sql

--- DBMS-I/Lab-10.sql
+++ DBMS-I/Lab-10.sql
@@ -1,8 +1,22 @@
-		--Lab_10	CREATE TABLE STUDENT_INFO(	RNO INT,	NAME VARCHAR(50),	BRANCH VARCHAR(50),	SPI DECIMAL(4,2),	BKLOG INT	)	INSERT INTO STUDENT_INFO VALUES	(101,'RAJU','CE',8.80,0),	(102, 'Amit', 'CE',2.20,3),
+		--Lab_10
+
+	CREATE TABLE STUDENT_INFO(
+	RNO INT,
+	NAME VARCHAR(50),
+	BRANCH VARCHAR(50),
+	SPI DECIMAL(4,2),
+	BKLOG INT
+	)
+
+	INSERT INTO STUDENT_INFO VALUES
+	(101,'RAJU','CE',8.80,0),
+	(102, 'Amit', 'CE',2.20,3),
 	(103, 'Sanjay', 'ME' ,1.50, 6),
 	(104, 'Neha' ,'EC' ,7.65 ,1),
 	(105, 'Meera', 'EE',5.52, 2),
-	(106, 'Mahesh', 'EC',4.50 ,3)	SELECT * FROM STUDENT_INFO
+	(106, 'Mahesh', 'EC',4.50 ,3)
+
+	SELECT * FROM STUDENT_INFO
 
 --	(PART-A)
 
@@ -54,7 +68,7 @@
 	WHERE RNO=104
 	SELECT * FROM ACADEMICDATA
 
---	(Part – B:)
+--	(Part â€“ B:)
 
 --1. Create a view that displays information of all students whose SPI is above 8.5
 	CREATE VIEW INFORMATION
@@ -79,7 +93,7 @@
 	WHERE BRANCH='CE'
 	SELECT * FROM COMPUTERVIEW
 
---(Part – C:)
+--(Part â€“ C:)
 
 --1. Create a view Result_EC that displays the name and SPI of students with SPI less than 5 of branch EC.
 	CREATE VIEW RESULT_EC
@@ -93,7 +107,7 @@
 	SET SPI=4.90
 	WHERE NAME='MAHESH'
 	SELECT * FROM RESULT_EC
---3. Create a view Stu_Bklog with RNo, Name and Bklog columns in which name starts with ‘M’ and having
+--3. Create a view Stu_Bklog with RNo, Name and Bklog columns in which name starts with â€˜Mâ€™ and having
 --bklogs more than 5.
 	CREATE VIEW STU_BKLOG
 	AS
@@ -101,4 +115,5 @@
 	FROM STUDENT_INFO
 	WHERE NAME LIKE 'M%' AND BKLOG>5
 	SELECT * FROM STU_BKLOG
---4. Drop Computerview form the database.	DROP VIEW COMPUTERVIEW
+--4. Drop Computerview form the database.
+	DROP VIEW COMPUTERVIEW
